611b5d3771d75ff3d0822d6a8c033dee783e6293
zhamilya <jozefineme@gmail.com>
2020-10-06 22:26:51 +0200

    Update basics.R
    hist()

--- different/basics.R
+++ different/basics.R
@@ -40,3 +40,46 @@ plot(dnorm, -3, +3,
      main= "standart normal distribution",
      xlab = "z-scores",
      ylab = "Density")
+
+#bar charts
+head(mtcars)
+nrow(mtcars)
+barplot(mtcars) #doesn't work
+cylinders <- table(mtcars$cyl)
+barplot(cylinders)
+plot(cylinders)
+
+# histogram - for quantitative, 
+# scaled, measured, interval or ratio level
+# see what you have
+# Shape, Gaps, Outliers, Symmetry
+
+head(iris)
+hist(iris$Sepal.Length)
+hist(iris$Sepal.Width)
+hist(iris$Petal.Length)
+hist(iris$Petal.Width)
+
+par(mfrow = c(3,1))
+hist(iris$Petal.Width [iris$Species == "setosa"], #[] condition
+     xlim = c(0,3),
+     breaks = 9, #how many bars
+     main = "Petal Width for Setosa",
+     xlab = "",
+     col = "red")
+
+hist(iris$Petal.Width [iris$Species == "versicolor"], #[] condition
+     xlim = c(0,3),
+     breaks = 9, #how many bars
+     main = "Petal Width for Versicolor",
+     xlab = "",
+     col = "purple")
+
+hist(iris$Petal.Width [iris$Species == "virginica"], #[] condition
+     xlim = c(0,3),
+     breaks = 9, #how many bars
+     main = "Petal Width for Virginica",
+     xlab = "",
+     col = "blue")
+#restore graphic parameter
+par(mfrow = c(1,1))
